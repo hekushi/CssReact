6335b3750e74553b0939c351338def42f642f1bf
hekushi <78855499+hekushi@users.noreply.github.com>
2021-12-20 08:10:40 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { InlineStyle } from "./componens/InlineStyle";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <InlineStyle />
+    </div>
+  );
+}
